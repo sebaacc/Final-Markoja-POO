c82b30ea9708bc00387d180cbdf325411c7935d7
Sebastián-Markoja <sebaacc11@gmail.com>
2022-12-06 19:55:04 -0300

    finalice ServicioSimple

--- src/Clases/ServicioSimple.java
+++ src/Clases/ServicioSimple.java
@@ -1,7 +1,29 @@
 package Clases;
 
-public class ServicioSimple {
+public class ServicioSimple extends Servicio {
     //Atributos
-
+    private double precioBase;
+    private boolean incluyeColocacion;
     //Métodos
+    @Override
+    public double calcularPrecio() {
+        if (incluyeColocacion == true){
+            return (getPrecioBase() + (getPrecioBase()*0.10));
+        } else {
+            return getPrecioBase();
+        }
+    }
+    //Getters y setters
+    public double getPrecioBase() {
+        return precioBase;
+    }
+    public void setPrecioBase(double precioBase) {
+        this.precioBase = precioBase;
+    }
+    public boolean isIncluyeColocacion() {
+        return incluyeColocacion;
+    }
+    public void setIncluyeColocacion(boolean incluyeColocacion) {
+        this.incluyeColocacion = incluyeColocacion;
+    }
 }
